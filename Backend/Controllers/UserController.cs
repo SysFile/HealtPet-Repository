cd15d3268410417d97c663c426a047e9d309af29
SebastianVilla-13 <85305705+SebastianVilla-13@users.noreply.github.com>
2021-09-20 15:49:43 -0500

    guardado temporal

---
+++ Backend/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using Backend.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    
+    public class UserController : ControllerBase
+    {
+
+        
+        
+    }
+}
