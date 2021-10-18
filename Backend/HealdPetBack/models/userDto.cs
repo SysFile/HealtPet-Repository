461a469505d98037384afac09fa6e15f9f432433
SebastianVilla-13 <85305705+SebastianVilla-13@users.noreply.github.com>
2021-10-18 14:09:05 -0500

    s

--- Backend/HealdPetBack/models/userDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Backend.models
-{
-    public class userDto
-    {
-        public int id { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
-    }
-}
