5108b054e5cbd4ed536a8ace38b68ba9de388f61
SebastianVilla-13 <85305705+SebastianVilla-13@users.noreply.github.com>
2021-11-23 23:42:03 -0500

    prueba

--- health/health/Controllers/pets/PetController.cs
+++ health/health/Controllers/pets/PetController.cs
@@ -4,6 +4,7 @@ using health.models.resp;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,7 @@ using System.Threading.Tasks;
 
 namespace health.Controllers.pets
 {
-    [Route("api/[controller]")]
+    
     [ApiController]
     [Authorize]
     public class PetController : ControllerBase
@@ -25,12 +26,113 @@ namespace health.Controllers.pets
         }
 
         [HttpGet]
-        public Object Get()
+        [Route("api/getListaMascotas")]
+        public Object GetListaMascotas()
         {
-
             return context.Mascotas.ToList();
-
         }
+
+        [HttpGet]
+        [Route("api/getMascotas/{id}")]
+        public Object GetMascotas(int id)
+        {
+            return context.PetsDetails.Where(u => u.idUsuario == id);
+        }
+
+        [HttpPost]
+        [Route("api/registerMascota")]
+        public Object RegisterMascota([FromBody] Pets pets)
+        {
+            Resp res = new Resp();
+
+            res.Ok = false;
+
+            try
+            {
+
+                if (pets != null)
+                {
+                    context.Mascotas.Add(pets);
+                    context.SaveChanges();
+                    res.Ok = true;
+                    res.data = pets;
+                    res.mensaje = "Registrado Exitosamente";
+                }
+                else
+                {
+                    res.mensaje = "Error Al Registrar";
+                }
+            }
+            catch (Exception e)
+            {
+                res.mensaje = "Error El Servidor";
+            }
+            return res;
+        }
+
+        [HttpPut]
+        [Route("api/updateMascota/{id}")]
+        public object UpdateMascota([FromBody] Pets pets, int id)
+        {
+            Resp res = new Resp();
+            res.Ok = false;
+            try
+            {
+                if (id == pets.idMascota)
+                {
+                    context.Entry(pets).State = EntityState.Modified;
+                    context.SaveChanges();
+                    res.Ok = true;
+                    res.data = pets;
+                    res.mensaje = "Actualizado con exito";
+                }
+                else
+                {
+                    res.mensaje = "Error al actualizar";
+                }
+            }
+            catch (Exception)
+            {
+                res.mensaje = "Error en el servidor";
+            }
+            return res;
+        }
+
+        [HttpDelete]
+        [Route("api/deleteMascota/{id}")]
+        public object DeleteMascota(int id)
+        {
+            Resp res = new Resp();
+            res.Ok = false;
+            try
+            {
+
+                var lst = context.Mascotas.SingleOrDefault(u => u.idMascota == id);
+
+                if (lst != null)
+                {
+                    context.Mascotas.Remove(lst);
+                    context.SaveChanges();
+                    res.Ok = true;
+                    res.data = lst;
+                    res.mensaje = "Eliminado Con Exito";
+                }
+                else
+                {
+                    res.mensaje = "Error al Eliminar";
+                }
+            }
+            catch (Exception)
+            {
+                res.mensaje = "Error en el servidor";
+            }
+            return res;
+        }
+
+
+
+
+
 
     }
 }
